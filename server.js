1cdd7a0587ffba99a5b3316ba227c7c7f8e29698
Renee Du <75010971+ReneeDu320@users.noreply.github.com>
2022-12-06 22:00:35 -0500

    hw4

---
+++ server.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+import minimist from "minimist";
+import express from "express";
+import { roll } from "./lib/roll.js";
+
+const args = minimist(process.argv.slice(2));
+const port = args.port || 5000;
+
+const app = express();
+
+app.get('/app/', (req, res) => {
+  res.send('200 OK');
+})
+
+app.get('/app/roll/', (req, res) => {
+  res.send(roll(6, 2, 1));
+})
+
+app.post('/app/roll/', express.urlencoded({extended: true}), (req, res) => {
+  res.send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls)));
+})
+
+app.get('/app/roll/:sides/', (req, res) => {
+  res.send(roll(parseInt(req.params.sides), 2, 1));
+})
+
+app.get('/app/roll/:sides/:dice/', (req, res) => {
+  res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1));
+})
+
+app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
+  res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)));
+})
+
+app.get('*', (req, res) => {
+  res.status(404).send('404 NOT FOUND');
+})
+
+app.listen(port);
